95047260fdbc3022eca8853fb7db37e4da020a89
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-03-10 09:21:41 -0800

    Patches

--- R5Revealed/140564059417346049.py
+++ R5Revealed/140564059417346049.py
@@ -1,10 +1,11 @@
 def avocadoCheck(avocados, testAvocado): return testAvocado%256 not in avocados and testAvocado >= 0
 def tlow5(avocados, spoon_size):
     if not avocados: return 0
-    elif spoon_size < 0: return 256
+    elif spoon_size < 0: return 255
     else:
         factors = [ x for x in range(avocados[::-1][0]-1,1,-1) if avocados[::-1][0]%x == 0 ]
         if factors and avocadoCheck(avocados, factors[0]): return 0
         elif avocadoCheck(avocados, avocados[::-1][0]**2): return 1
         elif avocadoCheck(avocados, avocados[::-1][0]-spoon_size): return 2
         else: return 3
+
--- R5Revealed/_zM_dummy.lua
+++ R5Revealed/_zM_dummy.lua
@@ -1,10 +1,11 @@
-function contains (tbl, object) for i = 1, #tbl, 1 do if tbl[i] == object then return true; end end return false; end
-function slice (avocado_count) if contains({0, 1}, avocado_count) then return false; end 
+function contains(tbl, object) for i = 1, #tbl, 1 do if tbl[i] == object then return true; end end return false; end
+function slice(avocado_count) if contains({0, 1}, avocado_count) then return false; end 
 	for i = avocado_count - 1, 2, -1 do if avocado_count % i == 0 then return i; end end return false; end
-function mash (avocado_count) return (avocado_count ^ 2) % 256; end
-function eat (avocado_count, spoon) return (avocado_count - spoon) >= 0 and (avocado_count - spoon) or false; end
-function trap_bot (avocados, spoon) if #avocados == 0 then return math.random(1, 32); elseif spoon <= 0 then return math.random(1, 32); end
+function mash(avocado_count) return (avocado_count ^ 2) % 256; end
+function eat(avocado_count, spoon) return (avocado_count - spoon) >= 0 and (avocado_count - spoon) or false; end
+function trap_bot(avocados, spoon) if #avocados == 0 then return math.random(1, 32); elseif spoon <= 0 then return math.random(1, 32); end
 	last = avocados[#avocados]; if not contains(avocados, slice(last)) and slice(last) then return 0;
 	elseif not contains(avocados, mash(last)) then return 1; 
 	elseif (not contains(avocados, eat(last, spoon))) and eat(last, spoon) >= 0 then return 2; 
 	else return 3; end end
+
