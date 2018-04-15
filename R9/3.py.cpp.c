2b6bdaf4d984de96f6aac0874152389893280d1b
Hans Shi <24215006+hanss314@users.noreply.github.com>
2018-04-15 10:33:49 -0700

    Edit

--- R9/3.py.cpp.c
+++ R9/3.py.cpp.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define p char*tlow(char*str){char*c
-p="#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#define p char*tlow(char*str){char*c%cp=%c%s%c;%c#/*%cdef tlow(str):return str or p%%(10,10,10,10,34,p,34,10,10,10);'''*/%cif(*str!=0)return str;char*r=(char*)malloc(575);sprintf(r,c,10,10,10,10,34,c,34,10,10,10);return r;}/*'''#*/";
+#define p char*tlow(char*str){char c[]
+p="#include <stdio.h>%c#include <stdlib.h>%c#define p char*tlow(char*str){char c[]%cp=%c%s%c;%c#/*%cdef tlow(str): return str or p%%(10,10,10,34,p,34,10,10,10,10,10,10);'''*/%cif(*str != 0) return str;%cchar *r = (char*)malloc(575);%csprintf(r,c,10,10,10,34,c,34,10,10,10,10,10,10);%creturn r;}/*'''#*/";
 #/*
-def tlow(str):return str or p%(10,10,10,10,34,p,34,10,10,10);'''*/
-if(*str!=0)return str;char*r=(char*)malloc(575);sprintf(r,c,10,10,10,10,34,c,34,10,10,10);return r;}/*'''#*/
+def tlow(str): return str or p%(10,10,10,34,p,34,10,10,10,10,10,10);'''*/
+if(*str != 0) return str;
+char *r = (char*)malloc(575);
+sprintf(r,c,10,10,10,34,c,34,10,10,10,10,10,10);
+return r;}/*'''#*/
