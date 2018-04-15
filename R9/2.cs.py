b541376649b9811b8a5987aede2ca37bc9fe9095
Hans Shi <24215006+hanss314@users.noreply.github.com>
2018-04-15 10:41:03 -0700

    remove trailings

--- R9/2.cs.py
+++ R9/2.cs.py
@@ -8,5 +8,3 @@ static string quine(string input){
 def quine(x):
   data = "#if false \"\"\"#endifstatic string quine(string input){{    string data = \"{0}\";    return input == \"\" ? string.Format(data, data) : input;    }}/*\"\"\"def quine(x):  data = \"{0}\"  return x if x is not '' else data.replace(\"{{{{\", \"{{\", 4).replace(\"}}}}\",\"}}\", 4).replace(\"{0}\", data, 2)"
   return x if x is not '' else data.replace("{{", "{", 4).replace("}}","}", 4).replace("{0}", data, 1)
-
-
