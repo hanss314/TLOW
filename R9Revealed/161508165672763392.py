1682f989113c12a1704f9e40589975192cfd54a4
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-04-19 16:49:42 -0700

    Finale

---
+++ R9Revealed/161508165672763392.py
@@ -0,0 +1,2 @@
+s = r"tlow9 = lambda n: n if n else 's = r\"' + s + '\"\n' + s.format(s)"
+tlow9 = lambda n: n if n else 's = r\"' + s + '\"\n' + s.format(s)
---
+++ R9Revealed/236257776421175296.cs.py
@@ -0,0 +1,12 @@
+#if false 
+""" 
+#endif
+static string quine(string input){
+    string data = "#if false \"\"\"#endifstatic string quine(string input){{    string data = \"{0}\";    return input == \"\" ? string.Format(data, data) : input;    }}/*\"\"\"def quine(x):  data = \"{0}\"  return x if x is not '' else data.replace(\"{{{{\", \"{{\", 4).replace(\"}}}}\",\"}}\", 4).replace(\"{0}\", data, 2)";
+    return input == "" ? string.Format(data, data) : input;
+    }/*"""
+def quine(x):
+  data = "#if false \"\"\"#endifstatic string quine(string input){{    string data = \"{0}\";    return input == \"\" ? string.Format(data, data) : input;    }}/*\"\"\"def quine(x):  data = \"{0}\"  return x if x is not '' else data.replace(\"{{{{\", \"{{\", 4).replace(\"}}}}\",\"}}\", 4).replace(\"{0}\", data, 2)"
+  return x if x is not '' else data.replace("{{", "{", 4).replace("}}","}", 4).replace("{0}", data, 1)
+
+
