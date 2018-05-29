2aebb92b329814bc14c8c0a9bccbeff5b3406796
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-05-29 14:58:13 -0700

    oops forgot to update

--- S1/R0/18.hs
+++ S1/R0/18.hs
@@ -7,4 +7,4 @@ enc (x,y)
     | x `elem` lower = cycle lower !! (indexOf x lower + indexOf y (lower ++ upper))
     | x `elem` upper = cycle upper !! (indexOf x upper + indexOf y (lower ++ upper))
     | otherwise      = x
-vig string key = map enc (zip string (cycle key))
+vig string key = (map enc . zip string . cycle) key
